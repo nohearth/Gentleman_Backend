b54548179fe3ce68ab2ae6332fa8a94fbdd2a578
Sr-Wild <wilderytg@gmail.com>
2021-09-27 00:18:56 -0400

    Mod-addCListBootcamps

--- controller/CListBootcamps.js
+++ controller/CListBootcamps.js
@@ -1,5 +1,8 @@
 
+const { response } = require('express')
 const mListBootcamps = require('../models/MListBootcamps')
+const resp = require('../utils/responses')
+
 
 async function crearteListBootcamps(req,res){
     try {
@@ -9,17 +12,75 @@ async function crearteListBootcamps(req,res){
             idBootcamps:value.idBootcamps
         })
         if(valListBootcamps){
-            return res.status(400).json({
-                Ok:0,
-                Data:valListBootcamps,
-                Message:'Error, list exists'
-            })
+            return resp.makeResponsesError(res,"LFound")
         }
-
-    }catch (error) {
-        res.status(400).json({
-            Ok:0,
-            Message: error
+        const listBootcamps = new mListBootcamps({
+            idUser:value.idUser,
+            idBootcamps:value.idBootcamps,
+            isApply:value.isApply
         })
+        const saveListBootcamps = await listBootcamps.save()
+       resp.makeResponsesOkData(res,saveListBootcamps,"LCreated")
+    }catch (e) {
+        resp.makeResponsesException(res,e)}
+}
+
+async function getAllListBootcamps(req,res){
+    try {
+        const listBootcamps = await mListBootcamps.find()
+        resp.makeResponsesOkData(res,listBootcamps,"Success")
+    } catch (e) {
+        resp.makeResponsesException(res,e)
     }
+}
+
+async function getListBootcamps(req,res){
+    try {
+        const listBootcamps = await mListBootcamps.findOne({_id: req.params.id})
+        resp.makeResponsesOkData(res,listBootcamps,"Success")
+    } catch (e) {
+        resp.makeResponsesException(res,e)
+    }
+}
+
+async function updatelistBootcamps(req,res){
+    try{
+        const listBootcamps = await mListBootcamps.findOne({_id: req.params.id})
+        const data = req.body
+        if(!listBootcamps){
+            return resp.makeResponsesError(res,"LNotFound")
+        }
+        const saveListBootcamps= await mListBootcamps.updateOne({
+            idUser:data.idUser,
+            idBootcamps:data.idBootcamps
+        },{
+            $set: data
+        })
+        resp.makeResponsesOkData(res,saveListBootcamps,"Updated")
+
+    }catch(e){
+        resp.makeResponsesException(res,e)
+    }
+
+}
+async function deleteListBootcamps(req,res){
+    try {
+        const listBootcamps = await mListBootcamps.findOne({_id: req.params.id})
+        if(!listBootcamps){
+            return resp.makeResponsesError(res,"LNotFound")
+        }
+        const deleteListBootcamps = await mListBootcamps.deleteOne({
+            _id:listBootcamps._id
+        })
+        resp.makeResponsesOkData(res,deleteListBootcamps,"Updated")
+    } catch (e) {
+        resp.makeResponsesException(res,e)
+    }
+}
+module.exports={
+    crearteListBootcamps,
+    getAllListBootcamps,
+    getListBootcamps,
+    updatelistBootcamps,
+    deleteListBootcamps
 }
